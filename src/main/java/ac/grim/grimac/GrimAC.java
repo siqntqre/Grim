9777466d8b9bb13cf05be48564ca0b829e57698a
siqntqre <jxpcjx@gmail.com>
2025-04-08 13:49:33 -0600

    Update GrimAC.java

--- src/main/java/ac/grim/grimac/GrimAC.java
+++ src/main/java/ac/grim/grimac/GrimAC.java
@@ -1,6 +1,7 @@
 package ac.grim.grimac;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
 
 public final class GrimAC extends JavaPlugin {
 
@@ -17,5 +18,13 @@ public final class GrimAC extends JavaPlugin {
     @Override
     public void onEnable() {
         GrimAPI.INSTANCE.start(this);
+
+        // Register Geyser plugin message channel listener
+        getServer().getMessenger().registerIncomingPluginChannel(this, "geyser:handshake", (channel, player, message) -> {
+            if (channel.equals("geyser:handshake")) {
+                WhitelistManager.add(player.getUniqueId());
+                getLogger().info("Whitelisted Geyser player: " + player.getName());
+            }
+        });
     }
 }
