f83facc2dd0d0e0c291a7d32713b0c59ef547928
siqntqre <jxpcjx@gmail.com>
2025-04-08 13:50:26 -0600

    Create WhitelistManager.java

---
+++ src/main/java/ac/grim/grimac/WhitelistManager.java
@@ -0,0 +1,17 @@
+package ac.grim.grimac;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class WhitelistManager {
+    private static final Set<UUID> whitelisted = new HashSet<>();
+
+    public static void add(UUID uuid) {
+        whitelisted.add(uuid);
+    }
+
+    public static boolean isWhitelisted(UUID uuid) {
+        return whitelisted.contains(uuid);
+    }
+}
