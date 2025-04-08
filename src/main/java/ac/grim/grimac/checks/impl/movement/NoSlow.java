f285570be79917cd7b73d1ccdf7731d39063ce11
siqntqre <jxpcjx@gmail.com>
2025-04-08 13:54:36 -0600

    Update NoSlow.java

--- src/main/java/ac/grim/grimac/checks/impl/movement/NoSlow.java
+++ src/main/java/ac/grim/grimac/checks/impl/movement/NoSlow.java
@@ -8,12 +8,14 @@ import ac.grim.grimac.player.GrimPlayer;
 import ac.grim.grimac.utils.anticheat.update.PredictionComplete;
 import com.github.retrooper.packetevents.protocol.player.ClientVersion;
 
+// ✅ Import your whitelist manager
+import ac.grim.grimac.whitelist.WhitelistManager;
+
 @CheckData(name = "NoSlow", description = "Was not slowed while using an item", setback = 5)
 public class NoSlow extends Check implements PostPredictionCheck {
     double offsetToFlag;
     double bestOffset = 1;
-    // The player sends that they switched items the next tick if they switch from an item that can be used
-    // to another item that can be used.  What the fuck mojang.  Affects 1.8 (and most likely 1.7) clients.
+
     public boolean didSlotChangeLastTick = false;
     public boolean flaggedLastTick = false;
 
@@ -23,11 +25,12 @@ public class NoSlow extends Check implements PostPredictionCheck {
 
     @Override
     public void onPredictionComplete(final PredictionComplete predictionComplete) {
+        // ✅ Skip check if player is whitelisted (Eagler/Geyser)
+        if (WhitelistManager.isWhitelisted(player.getUUID())) return;
+
         if (!predictionComplete.isChecked()) return;
 
-        // If the player was using an item for certain, and their predicted velocity had a flipped item
         if (player.packetStateData.isSlowedByUsingItem()) {
-            // 1.8 users are not slowed the first tick they use an item, strangely
             if (player.getClientVersion().isOlderThanOrEquals(ClientVersion.V_1_8) && didSlotChangeLastTick) {
                 didSlotChangeLastTick = false;
                 flaggedLastTick = false;
